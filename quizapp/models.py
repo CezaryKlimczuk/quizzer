a616cfbd6d8c5bcde928ea289c2b9810db8d9702
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-06 15:03:27 +0000

    Created Quiz and Question models

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    ansewer_text = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    answered_times = models.IntegerField(default=0)
+    correct_answered_times = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question_text
