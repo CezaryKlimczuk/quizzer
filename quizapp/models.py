890eb264646539772799cb9016894a101c9b0359
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-06 15:30:50 +0000

    Updated the quiz model with meta data

--- quizapp/models.py
+++ quizapp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Quiz(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    times_taken = models.IntegerField(default=0)
+    high_score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
