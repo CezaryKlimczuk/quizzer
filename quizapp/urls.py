033fd383255e4c13a09be48a55d67b6fd8a3f11d
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-06 15:55:51 +0000

    Added some basic listing of quizzes on the homepage and quiz views

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, quiz
 
 urlpatterns = [
     path('', index, name='index'),
+    path('quiz/<int:quiz_id>/', quiz, name='quiz'),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Quiz, Question
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    quizes = Quiz.objects.all()
+    return render(request, 'index.html', context={'quizzes': quizes})
+
+def quiz(request, quiz_id: int):
+    quiz_questions = Question.objects.filter(quiz=quiz_id)
+    return render(request, 'quiz.html', context={'questions': quiz_questions})
