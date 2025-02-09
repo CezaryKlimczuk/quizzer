bdfde766d600866815054cfa79b6f89e9cabea10
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-09 17:28:45 +0000

    Added featured boolean fields to Category and Quiz

--- quizzes/models.py
+++ quizzes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+    description = models.TextField(null=True)
+    featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -9,6 +11,7 @@ class Category(models.Model):
 
 class Quiz(models.Model):
     name = models.CharField(max_length=40)
+    featured = models.BooleanField(default=False)
     category = models.ForeignKey(Category, related_name='quizzes', on_delete=models.CASCADE)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
