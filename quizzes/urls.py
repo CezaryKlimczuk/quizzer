a646a470db3af944771ecc7addb44bb57f55eaeb
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-09 20:07:26 +0000

    Created a category view

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, quiz
+from .views import home, category, quiz
 
 urlpatterns = [
     path('', home, name='home'),
+    path('category/<int:category_id>/', category, name='category'),
     path('quiz/<int:quiz_id>/', quiz, name='quiz'),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -8,6 +8,12 @@ def home(request):
     quizzes = Quiz.objects.filter(featured=True)
     return render(request, 'home.html', {'categories': categories, 'quizzes': quizzes})
 
+def category(request, category_id: int):
+    quizzes = Quiz.objects.filter(category=category_id)
+    featured_quizzes = Quiz.objects.filter(category=category_id, featured=True)
+    category = Category.objects.get(id=category_id)
+    return render(request, 'category.html', {'category': category, 'quizzes': quizzes, 'featured_quizzes': featured_quizzes})
+
 def quiz(request, quiz_id: int):
     quiz_questions = Question.objects.filter(quiz=quiz_id)
     return render(request, 'quiz.html', context={'questions': quiz_questions})
