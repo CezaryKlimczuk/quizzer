7a96a195e2ae89fa486d692f973765f50f71b7ad
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2025-02-20 20:55:57 +0000

    Updated Quiz django admin

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from .models import Question, Quiz, Category
 
+
+@admin.action(description="Make quiz featured within category")
+def add_to_category_featured(modeladmin, request, queryset):
+    queryset.update(category_featured=True)
+
+@admin.action(description="Remove quiz from featured within category")
+def remove_from_category_featured(modeladmin, request, queryset):
+    queryset.update(category_featured=False)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'times_taken', 'category_featured']
+    list_filter = ['category']
+    actions = [add_to_category_featured, remove_from_category_featured]
+
 admin.site.register(Question)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Category)
